ffbf97f0dccb88db91c919fde8644801a7ee7d6f
klbc <2572623151@qq.com>
2022-03-05 23:39:33 +0800

    Test06开解

---
+++ src/Test06.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+//数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+public class Test06 {
+    public static void main(String[] args) {
+
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> re = new ArrayList<>();
+
+        return re;
+
+    }
+}
